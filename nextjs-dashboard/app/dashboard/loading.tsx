e51671c0394a0f2bae22469376c72684875b81db
Daniel Marinelarena <danielmarinelarena@gmail.com>
2025-10-23 11:31:33 -0700

    added loading skeleton

--- nextjs-dashboard/app/dashboard/loading.tsx
+++ nextjs-dashboard/app/dashboard/loading.tsx
@@ -1,3 +1,5 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
 export default function Loading() {
-  return <div className="mt-4 text-gray-400">Loading dashboard...</div>;
+  return <DashboardSkeleton />;
 }
